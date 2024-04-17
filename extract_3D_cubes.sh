bafc1787e2302e5f97f6de34f962eba33365279e
icekang <naravich@mit.edu>
2024-04-16 23:19:10 -0400

    feat: update split and scale

--- extract_3D_cubes.sh
+++ extract_3D_cubes.sh
@@ -1,11 +1,12 @@
 readarray -t train_subset_elements < datasets/Unlabeled_OCT/train.txt
-readarray -t test_subset_elements < datasets/Unlabeled_OCT/test.txt
-combined=("${train_subset_elements[@]}" "${test_subset_elements[@]}")
+# readarray -t test_subset_elements < datasets/Unlabeled_OCT/test.txt
+# combined=("${train_subset_elements[@]}" "${test_subset_elements[@]}")
+combined=("${train_subset_elements[@]}")
 for subset in "${combined[@]}"
 do
     python -W ignore infinite_generator_3D.py \
     --fold $subset \
-    --scale 32 \
+    --scale 96 \
     --data /storage_bizon/bizon_imagedata/naravich/Unlabeled_OCT/ \
     --save /storage_bizon/naravich/Unlabeled_OCT_cubes/
 done
