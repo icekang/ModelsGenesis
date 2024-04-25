838b61165928cb2773ad74e31e8ddf0e1db6c1f8
Naravich Chutisilp <naravich@mit.edu>
2024-04-25 13:54:58 -0400

    fix: env vars not found

--- finetune_submission.sh
+++ finetune_submission.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # Loading the required module
-# source /etc/profile
-source /home/gridsan/nchutisilp/.bashrc
+source /etc/profile
+# source /home/gridsan/nchutisilp/.bashrc
 module load anaconda/2023a-pytorch
 
 # Activating the conda environment
 source activate genesis
 which python
+
+# Setup env variables nn_UNet
+export nnUNet_raw="/home/gridsan/nchutisilp/datasets/nnUNet_Datasets/nnUNet_raw"
+export nnUNet_preprocessed="/home/gridsan/nchutisilp/datasets/nnUNet_Datasets/nnUNet_preprocessed"
+export nnUNet_results="/home/gridsan/nchutisilp/datasets/nnUNet_Datasets/nnUNet_results"
+
 # Run the script
 python pytorch/Genesis_Finetune.py --config pytorch/configs/fine_tune_config.yaml
