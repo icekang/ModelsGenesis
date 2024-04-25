2c812c02542891154ae51b92e62a7700d568cac9
Naravich Chutisilp <naravich@mit.edu>
2024-04-25 04:37:16 -0400

    fix: using my own profile for job submission

--- finetune_submission.sh
+++ finetune_submission.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Loading the required module
-source /etc/profile
-module load anaconda/2023b
+# source /etc/profile
+source /home/gridsan/nchutisilp/.bashrc
+module load anaconda/2023a-pytorch
 
 # Activating the conda environment
 source activate genesis
-
+which python
 # Run the script
 python pytorch/Genesis_Finetune.py --config pytorch/configs/fine_tune_config.yaml
