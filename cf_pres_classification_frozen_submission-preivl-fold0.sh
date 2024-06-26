06b86d7fcdb761c5b0b42593f5a4e8c841574a99
Naravich Chutisilp <naravich@mit.edu>
2024-06-26 11:37:30 -0400

    chore: run classification on frozen nnUNet

---
+++ cf_pres_classification_frozen_submission-preivl-fold0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Activating the conda environment
+source activate genesis
+which python
+
+# Setup env variables nn_UNet
+export nnUNet_raw="/home/gridsan/nchutisilp/datasets/nnUNet_Datasets/nnUNet_raw"
+export nnUNet_preprocessed="/home/gridsan/nchutisilp/datasets/nnUNet_Datasets/nnUNet_preprocessed"
+export nnUNet_results="/home/gridsan/nchutisilp/datasets/nnUNet_Datasets/nnUNet_results"
+
+# Run the script
+python pytorch/nnUNet_Classification.py --config pytorch/configs/fine_tune_config-cf_pres_frozen-full-3d_32x160x128_fold0.yaml
